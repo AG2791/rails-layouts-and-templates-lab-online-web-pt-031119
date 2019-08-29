d42fc12b0715ca9b314c24ccc903eda6f4506b8e
AJ Guiles <ashiguiles@gmail.com>
2019-08-29 21:34:41 +0000

    Automatically backed up by Learn

--- app/controllers/StoreAdminController.rb
+++ app/controllers/StoreAdminController.rb
@@ -15,6 +15,7 @@ class StoreAdminController < ApplicationController
   def invoice
     render :layout => false
   end
+  
   #This action is assigned the default layout for the controller, admin, but we don't want it to use any layout at all (while also not affecting the layouts assigned to other actions in the controller).
   
 end
